fb85f3ecf094195b11a3807d0edc1e60246b8c1f
Jarol20cb <u20201b559@upc.edu.pe>
2024-08-25 02:55:35 -0500

    se agrego diseño a los formularios de recuperar contraseña

--- src/app/component/auth/request-password-reset/request-password-reset.component.ts
+++ src/app/component/auth/request-password-reset/request-password-reset.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { AuthService } from 'src/app/service/auth.service';
+import { DialogComponent } from '../../dialogo/dialog/dialog.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-request-password-reset',
@@ -12,7 +15,7 @@ export class RequestPasswordResetComponent {
   message: string | null = null;
   errorMessage: string | null = null;
 
-  constructor(private fb: FormBuilder, private authService: AuthService) {
+  constructor(private fb: FormBuilder, private authService: AuthService, private dialog: MatDialog, private router: Router) {
     this.requestForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]]
     });
@@ -24,13 +27,34 @@ export class RequestPasswordResetComponent {
         .subscribe({
           next: response => {
             this.message = 'El correo para restablecer la contraseña ha sido enviado.';
+            this.openDialog('Correo enviado', this.message); // Ajusta el título
             this.errorMessage = null;
           },
           error: error => {
-            this.errorMessage = 'Hubo un error al enviar el correo. Intenta nuevamente.';
+            this.errorMessage = 'El correo electronico no existe.';
+            this.openDialog('Error', 'Error ' + (error.error.message || error.message || 'Error desconocido.'));
             this.message = null;
           }
         });
     }
+  }
+
+  openDialog(title: string, message: string): void {
+    const images: { [key: string]: string } = {
+      'Correo enviado': 'assets/exito.png',
+      'Restablecimiento exitoso': 'assets/exito.png',
+      'Error': 'assets/koala-mareado.png'
+    };
+
+    const imageUrl = images[title] || 'assets/error.png';
+
+    this.dialog.open(DialogComponent, {
+      width: '250px',
+      data: { title, message, imageUrl },
+    });
+  }
+
+  cancel() {
+    this.router.navigate(['/login']);
   }
 }
--- src/app/component/auth/reset-password/reset-password.component.ts
+++ src/app/component/auth/reset-password/reset-password.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'src/app/service/auth.service';
+import { DialogComponent } from '../../dialogo/dialog/dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-reset-password',
@@ -18,11 +20,16 @@ export class ResetPasswordComponent implements OnInit {
     private fb: FormBuilder,
     private authService: AuthService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private dialog: MatDialog
   ) {
     this.resetForm = this.fb.group({
       newPassword: ['', [Validators.required, Validators.minLength(6)]]
     });
+  }
+
+  cancel() {
+    this.router.navigate(['/login']);
   }
 
   ngOnInit(): void {
@@ -44,16 +51,33 @@ export class ResetPasswordComponent implements OnInit {
             console.log('Respuesta del backend:', response);
             this.message = 'Contraseña restablecida exitosamente.';
             this.errorMessage = null;
-            this.router.navigate(['/login']);  // Redirige al login después de restablecer la contraseña
+            this.openDialog('Restablecimiento exitoso', this.message); // Ajusta el título
+            this.router.navigate(['/login']);
           },
           error: error => {
             console.error('Error en el restablecimiento:', error);
             this.errorMessage = 'Hubo un error al restablecer la contraseña. Intenta nuevamente.';
+            this.openDialog('Error', 'Error en el registro: ' + (error.error.message || error.message || 'Error desconocido.'));
             this.message = null;
           }
         });
     } else {
       this.errorMessage = 'Formulario inválido o faltante token.';
     }
+  }
+
+  openDialog(title: string, message: string): void {
+    const images: { [key: string]: string } = {
+      'Correo enviado': 'assets/exito.png',
+      'Restablecimiento exitoso': 'assets/exito.png',
+      'Error': 'assets/koala-mareado.png'
+    };
+
+    const imageUrl = images[title] || 'assets/error.png';
+
+    this.dialog.open(DialogComponent, {
+      width: '250px',
+      data: { title, message, imageUrl },
+    });
   }
 }
